3ebf103ac802bd56653a5e1094b1aa4f2793a695
Dan Farcas <dan.farcas@3pillarglobal.com>
2023-05-09 23:13:14 +0300

    dockerized things, added deploy juice

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-const express = require('express');
-const fs = require('fs');
-const uuid = require('uuid');
-const cors = require('cors');
+const express = require("express");
+const fs = require("fs");
+const uuid = require("uuid");
+const cors = require("cors");
 
-let articles = require('./articles.json');
+let articles = require("./articles.json");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const updateArticlesFile = (content) => (
-  fs.writeFile('./articles.json', JSON.stringify(content, null, 2), (err) => {
+const updateArticlesFile = (content) =>
+  fs.writeFile("./articles.json", JSON.stringify(content, null, 2), (err) => {
     if (err) throw err;
-    console.log('Articles file has been updated.');
+    console.log("Articles file has been updated.");
     articles = content;
-  })
-);
+  });
 
-app.get('/articles', (req, res) => {
+app.get("/articles", (req, res) => {
   return res.json(articles);
 });
 
-app.get('/articles/:articleId', (req, res) => {
+app.get("/articles/:articleId", (req, res) => {
   const { articleId } = req.params;
-  const article = articles.find(a => a.id === articleId);
+  const article = articles.find((a) => a.id === articleId);
 
   if (!article) {
     return res.status(404).send(`Article with ID ${articleId} not found.`);
@@ -36,11 +35,18 @@ app.get('/articles/:articleId', (req, res) => {
   return res.json(article);
 });
 
-app.post('/articles', (req, res) => {
-  const { price, title , imageUrl } = req.body;
+app.post("/articles", (req, res) => {
+  const { price, title, imageUrl } = req.body;
 
-  if (!price || typeof price !== 'number' || !title || typeof title !== 'string'|| !imageUrl || typeof imageUrl !== 'string') {
-    return res.status(400).send('Missing or invalid data.');
+  if (
+    !price ||
+    typeof price !== "number" ||
+    !title ||
+    typeof title !== "string" ||
+    !imageUrl ||
+    typeof imageUrl !== "string"
+  ) {
+    return res.status(400).send("Missing or invalid data.");
   }
 
   const id = uuid.v4();
@@ -50,15 +56,15 @@ app.post('/articles', (req, res) => {
   try {
     updateArticlesFile(newArticles);
   } catch (e) {
-    return res.status(500).send('A server error occured.');
+    return res.status(500).send("A server error occured.");
   }
 
   return res.json(newArticle);
 });
 
-app.put('/articles/:articleId', (req, res) => {
+app.put("/articles/:articleId", (req, res) => {
   const { articleId } = req.params;
-  const index = articles.findIndex(a => a.id === articleId);
+  const index = articles.findIndex((a) => a.id === articleId);
 
   if (index < 0) {
     return res.status(404).send(`ARTICLE with ID ${articleId} not found.`);
@@ -66,8 +72,15 @@ app.put('/articles/:articleId', (req, res) => {
 
   const { price, title, imageUrl } = req.body;
 
-  if (!imageUrl || typeof imageUrl !== 'string' || !price || typeof price !== 'number' || !title || typeof title !== 'string') {
-    return res.status(400).send('Missing or invalid data.');
+  if (
+    !imageUrl ||
+    typeof imageUrl !== "string" ||
+    !price ||
+    typeof price !== "number" ||
+    !title ||
+    typeof title !== "string"
+  ) {
+    return res.status(400).send("Missing or invalid data.");
   }
 
   const newArticle = { ...articles[index], price, imageUrl, title };
@@ -77,15 +90,15 @@ app.put('/articles/:articleId', (req, res) => {
   try {
     updateArticlesFile(newArticles);
   } catch (e) {
-    return res.status(500).send('A server error occured.');
+    return res.status(500).send("A server error occured.");
   }
 
   return res.json(newArticle);
 });
 
-app.delete('/articles/:articleId', (req, res) => {
+app.delete("/articles/:articleId", (req, res) => {
   const { articleId } = req.params;
-  const index = articles.findIndex(a => a.id === articleId);
+  const index = articles.findIndex((a) => a.id === articleId);
 
   if (index < 0) {
     return res.status(404).send(`Article with ID ${articleId} not found.`);
@@ -97,7 +110,7 @@ app.delete('/articles/:articleId', (req, res) => {
   try {
     updateArticlesFile(newArticles);
   } catch (e) {
-    return res.status(500).send('A server error occurred.');
+    return res.status(500).send("A server error occurred.");
   }
 
   return res.send(`Article with ID ${articleId} was deleted.`);
